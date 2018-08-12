1223734e653c2c7aacaf9a0b07ecd02e13361677
ThanmayaRaghuram <thanmaya@doordash.com>
2018-08-12 00:52:38 -0700

    remove import

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import styles from "./App.scss";
 import Game from "../Game";
 
 class App extends Component {
   render() {
     return (
-      <div >
+      <div>
         <Game />
       </div>
     );
